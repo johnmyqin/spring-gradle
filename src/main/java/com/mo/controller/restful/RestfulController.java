0ef99d4895ae09cfe7fb6964673e3a688d4487f3
moxingwang <xingwangmo@gmail.com>
2015-12-27 12:46:20 +0800

    can be load

---
+++ src/main/java/com/mo/controller/restful/RestfulController.java
@@ -0,0 +1,14 @@
+package com.mo.controller.restful;
+
+//import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>Title:  </p>
+ * <p>Date: 2015-12-26 20:39 </p>
+ * @author <a href="mailto: ">mo</a>
+ * @version 1.0.1
+ */
+//@RequestMapping("/fs")
+public class RestfulController {
+
+}
---
+++ src/main/java/com/mo/dao/mysql_user/UserDao.java
@@ -0,0 +1,14 @@
+package com.mo.dao.mysql_user;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>Title:  </p>
+ * <p>Date: 2015-12-27 12:17 </p>
+ *
+ * @author <a href="mailto: ">mo</a>
+ * @version 1.0.1
+ */
+@Repository
+public interface UserDao {
+}
